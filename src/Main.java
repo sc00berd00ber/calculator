47fa7e627050a416cf77d037bb4f50adc50736ae
Nat Tessema <natnaeltessema1@gmail.com>
2025-04-10 13:32:08 -0700

    update

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,43 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        //CREATE AN INSTANCE OF SCANNER OBJECT
+        //pass in input stream to use for reading
+        Scanner myScanner = new Scanner(System.in);
+
+        // get desired operator +-/*
+        // get two numbers,
+        // add them together,
+        // and display the sum
+
+        System.out.print("Enter a single operator.\ne.g. '+', '-', '*', or '/':\n");
+        String operator = myScanner.next(); //get string
+
+        System.out.print("Enter first number: ");
+        //read next integer from the input stream (console/terminal)
+        int num1 = myScanner.nextInt();
+
+        System.out.print("Enter second number: ");
+        //read second integer from the input stream AKA console
+        int num2 = myScanner.nextInt();
+
+        // total up the two together into a sum
+        int result = 0;
+
+        if(operator.equals("+")){
+            result = num1 + num2;
+        }else if(operator.equals("-")){
+            result = num1 - num2;
+        }else if(operator.equals("*")){
+            result = num1 * num2;
+        }else if(operator.equals("/")){
+            result = num1 / num2;
         }
+
+        System.out.println("The result is " + result);
     }
 }
